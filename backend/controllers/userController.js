a57e807196e1c005233b739d9bb16cf7e6d34ec3
Prajeet Thakur <np01cp4a190087@islingtoncollege.edu.np>
2022-08-05 19:13:42 +0545

    Register functionality integration with frontend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -30,7 +30,6 @@ const registerUser = asyncHandler(async (req, res) => {
         name,
         email,
         password: hashedPassword,
-        token: generateToken(user._id)
     })
 
     if (user) {
@@ -38,6 +37,7 @@ const registerUser = asyncHandler(async (req, res) => {
             _id: user.id,
             name: user.name,
             email: user.email,
+            token: generateToken(user._id),
         })
     } else {
         res.status(400)
